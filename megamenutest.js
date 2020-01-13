184f0d0c42ecd303df2f8e49b9d50cda45cd8d9c
lilliananderso <57363174+lilliananderso@users.noreply.github.com>
2020-01-12 22:23:33 -0600

    my pages just about done

--- megamenutest.js
+++ megamenutest.js
@@ -1,2 +1,39 @@
 $(document)
    .on('click.bs.dropdown.data-api', '.dropdown', function (e) { e.stopPropagation() })
+
+   // When the user clicks on div, open the popup
+   function myFunction() {
+     var popup = document.getElementById("myPopup");
+     popup.classList.toggle("show");
+   }
+
+   function myFunction2() {
+     var popup = document.getElementById("myPopup2");
+     popup.classList.toggle("show");
+   }
+
+   function myFunction3() {
+     var popup = document.getElementById("myPopup3");
+     popup.classList.toggle("show");
+   }
+
+   function myFunction4() {
+     var popup = document.getElementById("myPopup4");
+     popup.classList.toggle("show");
+   }
+
+
+  function Function1() {
+        var popup = document.getElementById("Popup1");
+        popup.classList.toggle("show");
+      }
+
+  function Function2() {
+        var popup = document.getElementById("Popup2");
+            popup.classList.toggle("show");
+      }
+
+  function Function3() {
+        var popup = document.getElementById("Popup3");
+            popup.classList.toggle("show");
+      }
